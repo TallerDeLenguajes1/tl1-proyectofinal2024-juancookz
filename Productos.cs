a8d7c870b3735f663535360ed78f95b01676ee9b
“juancookz” <jcavila383@gmail.com>
2024-06-13 17:28:51 -0300

    Prueba de API

--- Productos.cs
+++ Productos.cs
@@ -3,3 +3,8 @@ public class Producto
 public string Nombre { get; set; }
 public int Precio { get; set; }
 }
+public class Frase{
+    public string quote;
+    public string author
+    ;
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
 ﻿using System;
 using System.Text.Json;
+HttpClient client = new HttpClient();
+Console.Clear();
+string apiUrl = "https://api.breakingbadquotes.xyz/v1/quotes";
+HttpResponseMessage response = await client.GetAsync(apiUrl);
+response.EnsureSuccessStatusCode();
+string responseBody = await response.Content.ReadAsStringAsync();
+System.Console.WriteLine(responseBody);
+Frase frase = JsonSerializer.Deserialize<Frase>(responseBody);
+System.Console.WriteLine($"Frase: {frase.quote} Personaje: {frase.author}");
+
 //Creamos una instancia de producto
 Producto papas = new Producto
 {
@@ -8,7 +18,6 @@ Producto papas = new Producto
 };
 //Serializamos en JSON el producto y lo guardamos como un string
 string jsonString = JsonSerializer.Serialize(papas);
-Console.Clear();
 //Escribimos la cadena que se serializo
 Console.WriteLine(jsonString);
 //Ahora Deserealizamos la cadena JSNON y creamos un objeto Producto a partir de ella
