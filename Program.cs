1802785411399b39e1671da0b0237baea1962786
“juancookz” <jcavila383@gmail.com>
2024-06-13 13:16:16 -0300

    Demo JSON

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Text.Json;
+//Creamos una instancia de producto
+Producto papas = new Producto
+{
+    Nombre = "Papas Fritas",
+    Precio = 30
+};
+//Serializamos en JSON el producto y lo guardamos como un string
+string jsonString = JsonSerializer.Serialize(papas);
+Console.Clear();
+//Escribimos la cadena que se serializo
+Console.WriteLine(jsonString);
+//Ahora Deserealizamos la cadena JSNON y creamos un objeto Producto a partir de ella
+Producto producto = JsonSerializer.Deserialize<Producto>(jsonString);
+//Escribimos el producto que se deserializo
+Console.WriteLine($"Nombre: {producto.Nombre} Precio: {producto.Precio}");
