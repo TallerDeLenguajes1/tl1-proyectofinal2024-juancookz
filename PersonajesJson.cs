32b9dce94f8f192cb35b5111ee262086b4bedc67
juancookz <jcavila383@gmail.com>
2024-08-04 21:05:05 -0300

    Inicializacion

---
+++ PersonajesJson.cs
@@ -0,0 +1,14 @@
+class PersonajesJson
+{
+    public void GuardarPersonajes(List<Personaje> ListaPersonajes, string nombreArchivo)
+    {
+    }
+    public List<Personaje> LeerPersonajes(string nombreArchivo)
+    {
+        List<Personaje> ListaPersonajes;
+        return ;
+    }
+    public void Existe()
+    {
+    }
+}
